6296a9205b5ecfbfa85b900280ff5f6177435a49
Simar <ubhisimar17@gmail.com>
2024-07-23 11:05:15 -0500

    last login implemented

---
+++ WSC_Korea_2023/Module C and D/atp2/Backend/app/Listeners/LastLogin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LastLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event): void
+    {
+        // Works for both admin and users
+        $user = $event->user;
+
+        $user->last_login = now();
+        $user->save();
+    }
+}
